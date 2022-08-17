d5e818c9eae31e1e635f723e0be8d2d092e93118
jean-rodriguez kinouani <kinouani@gmail.com>
2022-08-17 11:26:22 +0200

    model and controller restaurant

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,21 @@
+class RestaurantsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+  def new
+  end
+
+  def update
+  end
+
+  def edit
+  end
+end
